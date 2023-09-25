ead3a7a3f9adfda9889b80814360348ee111dfe2
phongvanptit <phongvanptit@gmail.com>
2023-09-25 20:06:53 +0700

    Home work Day 2

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import CInput from './CInput';
-import { Component, useState } from 'react';
-import Game from './Game'; 
-
+import { Component, PureComponent, useState } from 'react';
+import Game from './Game';
+import Clock from './Clock';
+import withStyle from './withStyle';
 // function App() {
 //   const users = ["phong", "Minh", "nam"];
 //   let [seconds, setSecond] = useState(0);
@@ -24,35 +25,43 @@ import Game from './Game';
 // }
 
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       second: 0
     }
   }
+  componentDidMount() {
+    console.log("App did mount")
+  }
   setSecond = () => {
     setInterval(() => {
-      this.setState((prevState, props) => ({ second: prevState.second + 1 }));
+      this.setState((prevState, props) => ({ second: this.state.second + 1 }));
     }, 1000);
   };
 
   render() {
     let { second } = this.state;
+    const NewCInput = withStyle(CInput);
+    const ClockNew = withStyle(Clock);
     return (
       <div className="App">
         {/* Props sample */}
-        {/* <CInput label="Email" disabled={true} defaultValue="admin@gmail.com" />
-      <CInput label="Số điện thoại" defaultValue="8739873298768"/>
-      <CInput label="Chỗ ở hiện tại" defaultValue="Ha Noi"/>
-      <CInput label="Địa chỉ thường chú" defaultValue="Ha Nam"/> */}
+        {/* <CInput label="Email" disabled={true} defaultValue="admin@gmail.com" style={{ fontWeight: "bold" }} />
+        <CInput label="Số điện thoại" defaultValue="8739873298768" />
+        <CInput label="Chỗ ở hiện tại" defaultValue="Ha Noi" />
+        <CInput label="Địa chỉ thường chú" defaultValue="Ha Nam" /> */}
 
         {/* State sample */}
         {/* <h1>{second}</h1>
         <button onClick={() => this.setSecond()}>Start</button> */}
 
         {/* User define event */}
-        <Game />
+        {/* <Game /> */}
+        <ClockNew />
+        <NewCInput label="HOC" style={{ color: "blue" }} />
+        <CInput label="Chỗ ở hiện tại" defaultValue="Ha Noi" />
       </div >
     );
   }
